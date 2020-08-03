75004e1d77dffa01c4b30737f558246641829d5d
abdelo1 <abdeli12.ai@gmail.com>
2020-08-03 17:59:33 +0200

    rien

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -9,7 +9,7 @@ class Header extends Component {
         this.state={
             open:false
         }
-
+    this.toggleMenu=this.toggleMenu.bind(this)
     }
 
     componentWillMount() {
@@ -19,7 +19,11 @@ class Header extends Component {
     componentDidMount() {
 
     }
-
+    toggleMenu(){
+    this.setState({
+        open:!this.state.open
+    })
+}
     
 
     render() {
@@ -28,10 +32,11 @@ class Header extends Component {
             fontSize: 15,
             fontWeight:'bold'
           };
+         
         return (
             <>
             <div className=' header'>
-               <div className="container d-flex flex-row justify-content-between">
+               <div className=" d-flex flex-row justify-content-around">
                     <p className="d-flex flex-row "><img width={70} src={logo} alt="logo"/> <span className='mt-3 ' style={style}> GAZIC</span> </p>   
                     <div className='mt-3 d-flex flex-row '>
                         <div class="form-group position-relative col-md-12">
@@ -52,31 +57,30 @@ class Header extends Component {
                                         <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                                     </svg>
                                 </div>
-                                <div className='logo_caret mt-1'>
+                                {this.state.open ? 
+                                 <div onClick={this.toggleMenu} className='logo_caret mt-1'>
                                     <svg width="0.7em" height="0.7em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                                     </svg>
-                                </div>
-                                <div className='logo_caret mt-1'>
+                                </div> : <div onClick={this.toggleMenu} className='logo_caret mt-1'>
                                     <svg width="0.7em" height="0.7em" viewBox="0 0 16 16" class="bi bi-chevron-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
                                     </svg>
                                 </div>
+                                }
+                               
+                               
                                 <div className='logo_person'>
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                                </svg>
-                                </div>
-                                <div className='logo_person'>
-                                    <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="ml-2 bi bi-cart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                                     </svg>
                                 </div>
                             </div>
+                          
+                            {this.state.open &&  
                             <div className='menu'>
-                              <a href='' className="a_menu">Se connecter</a>
-                            </div>
-
+                            <a href='' className="a_menu">Se connecter</a>
+                            </div>}
                         </div>
                     </div>
                </div>
